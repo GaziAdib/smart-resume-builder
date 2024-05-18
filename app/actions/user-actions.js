d6c49e3f484b519e586040a89000c98e5a549d43
unknown <adibcse10@gmail.com>
2024-05-18 19:23:18 +0600

    added user based delete all the sections by id using server action for now

--- app/actions/user-actions.js
+++ app/actions/user-actions.js
@@ -130,3 +130,147 @@ export const fetchSetting = async () => {
     }
     
 }
+
+
+// Delete Skill 
+
+export const deleteSkill = async (skillId) => {
+    const session = await getServerSession(authOptions);
+    try {
+        if(session?.user?.role ==='USER') {
+
+            const mySkill = await prisma.skill.findFirst({
+                where: {
+                    id: skillId
+                }
+            })
+            
+            if (session?.user?.id === mySkill.userId) {
+                await prisma.skill.delete({
+                    where: {
+                        id: skillId
+                    }
+                })
+            }
+    
+        }
+    } catch (error) {
+        console.log('error in Deleting Skill', error);
+    }
+    
+}
+
+
+// Delete Education Info By Id
+
+export const deleteEducation = async (educationId) => {
+    const session = await getServerSession(authOptions);
+    try {
+        if(session?.user?.role ==='USER') {
+
+            const myEducation = await prisma.educationalQualification.findFirst({
+                where: {
+                    id: educationId
+                }
+            })
+            
+            if (session?.user?.id === myEducation.userId) {
+                await prisma.educationalQualification.delete({
+                    where: {
+                        id: educationId
+                    }
+                })
+            }
+    
+        }
+    } catch (error) {
+        console.log('error in Deleting Education', error);
+    }
+    
+}
+
+
+// Delete work Experience by Id
+
+export const deleteWorkExperience = async (experienceId) => {
+    const session = await getServerSession(authOptions);
+    try {
+        if(session?.user?.role ==='USER') {
+
+            const myExperience = await prisma.experience.findFirst({
+                where: {
+                    id: experienceId
+                }
+            })
+            
+            if (session?.user?.id === myExperience.userId) {
+                await prisma.experience.delete({
+                    where: {
+                        id: experienceId
+                    }
+                })
+            }
+    
+        }
+    } catch (error) {
+        console.log('error in Deleting Work Experience', error);
+    }
+    
+}
+
+// DELETE Reference
+
+export const deleteReference = async (referenceId) => {
+    const session = await getServerSession(authOptions);
+    try {
+        if(session?.user?.role ==='USER') {
+
+            const myReference = await prisma.reference.findFirst({
+                where: {
+                    id: referenceId
+                }
+            })
+            
+            if (session?.user?.id === myReference.userId) {
+                await prisma.reference.delete({
+                    where: {
+                        id: referenceId
+                    }
+                })
+            }
+    
+        }
+    } catch (error) {
+        console.log('error in Deleting Work Refenrence', error);
+    }
+    
+}
+
+
+// DELETE Resume Info
+
+export const deleteFullResumeInfo = async (resumeId) => {
+    const session = await getServerSession(authOptions);
+    try {
+        if(session?.user?.role ==='USER') {
+
+            const myResume = await prisma.resume.findFirst({
+                where: {
+                    id: resumeId
+                }
+            })
+            
+            if (session?.user?.id === myResume.userId) {
+                await prisma.resume.delete({
+                    where: {
+                        id: resumeId
+                    }
+                })
+            }
+    
+        }
+    } catch (error) {
+        console.log('error in Deleting Resume Info', error);
+    }
+    
+}
